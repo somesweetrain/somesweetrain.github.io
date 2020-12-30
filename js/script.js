b4593f74d27d0ac7ee8b507541e0d5210c9fdc8d
rain <rain.murkhain@fortumo.com>
2020-12-30 15:21:23 +0200

    add bid showing logic

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
-const data = [
+const all_bids = [
   {
-    bid: "1c", value: "18+pp", next:[
-      {bid: "1d", value: "0-7pp"}
+    bid: "1c", value: "17+pp", next:[
+      {bid: "1d", value: "0-7pp", next:[
+          {bid: "1h", value: "20+pp"}
+        ]
+      }
+    ]
+  },
+  {
+    bid: "1d", value: "11-16pp", next:[
+      {bid: "1h", value: "7+pp 4+h", next:[
+          {bid: "1s", value: "tavaliselt bluff"}
+        ]
+      }
     ]
   }
 ];
 
 let listDiv = document.getElementById('bids');
 
-list_bids(data, listDiv);
+list_bids(all_bids, listDiv);
 
 function list_bids(data, parent_html_element) {
 
   for (let i = 0; i < data.length; ++i) {
+
     let ul=document.createElement('ul');
     let li=document.createElement('li');
     li.innerHTML = data[i].bid + " - " + data[i].value;
 
-    if (data[i].next) {
-      list_bids(data[i].next, li);
-    }
+    li.onclick = function() { // onclick function to show child elements
+
+      if (data[i].next) {
+        list_bids(data[i].next, this);
+        console.log(data[i].next);
+      }
+
+      li.onclick = function () { } // if list element has been clicked once, then we remove onclick effect
+    };
 
     ul.appendChild(li);
     parent_html_element.appendChild(ul);
